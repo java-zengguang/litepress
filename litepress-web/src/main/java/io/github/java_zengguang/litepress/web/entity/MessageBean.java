ba9e02e0ea6a69453e37e5b894a53e4f719d7f02
zengguang-phq <zengguang-phq@sinosig.com>
2025-09-02 15:53:20 +0800

    调整pom

--- litepress-web/src/main/java/io/github/java_zengguang/litepress/web/entity/MessageBean.java
+++ litepress-web/src/main/java/io/github/java_zengguang/litepress/web/entity/MessageBean.java
@@ -6,20 +6,23 @@ import io.github.java_zengguang.litepress.core.bean.entity.PageEntity;
 /**
  * Created by Administrator on 2019/2/12 0012.
  */
-public class MessageBean extends MainModel {
+public class MessageBean<T> extends MainModel {
     public String message;
     public boolean success;
-    public Object object;
+    public T object;
 
     public PageEntity pageEntity;
 
-    public MessageBean(String message, boolean success, Object object) {
+    public MessageBean() {
+    }
+
+    public MessageBean(String message, boolean success, T object) {
         this.message = message;
         this.success = success;
         this.object = object;
     }
 
-    public MessageBean(String message, boolean success, Object object, PageEntity pageEntity) {
+    public MessageBean(String message, boolean success, T object, PageEntity pageEntity) {
         this.message = message;
         this.success = success;
         this.object = object;
